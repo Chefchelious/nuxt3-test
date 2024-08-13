42ddf30fdb06a6aa69e39b01aa9e6d610d3defc7
Dmitrii Morrows <trollman1993@gmail.com>
2024-08-13 17:24:32 +0600

    select added

--- components/shared/Product/ProductList.vue
+++ components/shared/Product/ProductList.vue
@@ -1,31 +1,50 @@
 <template>
-<Container>
-  <div class="product-list">
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-    <ProductCard />
-  </div>
-</Container>
+  <Container>
+    <div class="select-wrapper">
+      <q-select
+        v-model="modelValue"
+        :options="['one', 'two', 'three']"
+        class="col-3 q-pa-none field-36"
+        style="border: 1px solid #DBDBDB; border-radius: 12px; background-color: #EDEDED"
+        borderless
+        popup-content-class="bg-grey-2"
+      >
+        <template v-if="!modelValue" #selected>
+          <div class="text-weight-regular text-black placeholder-size">Все товары и услуги</div>
+        </template>
+      </q-select>
+    </div>
+    <div class="product-list">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </div>
+  </Container>
 </template>
 
 <script setup lang="ts">
-
+const modelValue = ref('');
 </script>
 
 <style scoped lang="scss">
+.select-wrapper {
+  max-width: 443px;
+  margin: 26px auto;
+}
+
 .product-list {
   display: grid;
   grid-template-columns: repeat(4, 1fr);
